27fbef7d28ad94763b334d71a3586cbf3c6255dc
hmhm903 <hmhm903m@gmail.com>
2017-10-28 13:14:21 +0900

    Imprement functions of stone class.

--- reversi/stone.cpp
+++ reversi/stone.cpp
@@ -1,6 +1,23 @@
 #include "stone.h"
 
-Stone::Stone()
+Stone::Stone(STONE_COLOR color)
 {
+    this->color_ = color;
+    return;
+}
 
+STONE_COLOR Stone::GetStoneColor()
+{
+    return color_;
+}
+
+void Stone::SetStoneColor(STONE_COLOR color)
+{
+    this->color_ = color;
+    return;
+}
+
+void Stone::Reverse()
+{
+    color_ = color_ == STONE_COLOR::BLACK ? STONE_COLOR::WHITE : STONE_COLOR::BLACK;
 }
--- reversi/stone.h
+++ reversi/stone.h
@@ -1,11 +1,24 @@
 #ifndef STONE_H
 #define STONE_H
 
+enum class STONE_COLOR
+{
+    BLACK,
+    WHITE
+};
 
 class Stone
 {
+private:
+    STONE_COLOR color_;
+
 public:
-    Stone();
+    Stone(STONE_COLOR color);
+
+    STONE_COLOR GetStoneColor();
+    void SetStoneColor(STONE_COLOR color);
+
+    void Reverse();
 };
 
 #endif // STONE_H

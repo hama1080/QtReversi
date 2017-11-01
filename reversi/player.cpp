332946222718a027015e161bf6b0c8cfb3cf7a67
hmhm903 <hmhm903m@gmail.com>
2017-11-01 22:33:07 +0900

    Implement player class.

--- reversi/player.cpp
+++ reversi/player.cpp
@@ -1,6 +1,18 @@
 #include "player.h"
 
-Player::Player()
+Player::Player(STONE_COLOR color)
 {
+	color_ = color;
+	return;
+}
 
+void Player::SetPlayerColor(STONE_COLOR color)
+{
+	color_ = color;
+	return;
+}
+
+STONE_COLOR Player::GetPlayerColor()
+{
+	return color_;
 }
--- reversi/player.h
+++ reversi/player.h
@@ -1,11 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "stone.h"
 
+class Vector2d;
 class Player
 {
+private:
+	STONE_COLOR color_;
+
 public:
-    Player();
+	Player(STONE_COLOR color);
+	
+	void SetPlayerColor(STONE_COLOR color);
+	STONE_COLOR GetPlayerColor();
+	virtual Vector2d PutStone() = 0;
 };
 
 #endif // PLAYER_H

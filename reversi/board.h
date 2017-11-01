b478e418dc5328c604dd5c4ba2f110819756549a
hmhm903 <hmhm903m@gmail.com>
2017-11-01 22:42:59 +0900

    Update the definition of board class.

--- reversi/board.h
+++ reversi/board.h
@@ -1,11 +1,24 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <map>
+#include "stone.h"
+using namespace std;
+
+class Vector2d;
+class Cell;
 
 class Board
 {
+private:
+	map<Vector2d, Cell> board_;
+	map<STONE_COLOR, unsigned int> stone_cnt_; // Express the number of each stone.
+
 public:
     Board();
+	
+	void Initialize();
+	void PutStone(Vector2d pos);
 };
 
 #endif // BOARD_H

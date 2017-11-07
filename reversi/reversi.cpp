0507073b161b63b145606622045c4786833ab350
hmhm903 <hmhm903m@gmail.com>
2017-11-07 19:29:34 +0900

    Imprement Initialize() of Reversi class.

---
+++ reversi/reversi.cpp
@@ -0,0 +1,21 @@
+#include "reversi.h"
+#include "board.h"
+#include "human.h"
+
+Reversi::Reversi()
+{
+	Initialize();
+	return;
+}
+
+Reversi::~Reversi()
+{
+
+}
+
+void Reversi::Initialize()
+{
+	board_ = new Board();
+	player_list_.push_back(new Human(STONE_COLOR::BLACK));
+	player_list_.push_back(new Human(STONE_COLOR::WHITE));
+}

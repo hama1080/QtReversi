0b374e7ecab2c7e867aa3d353c5963bfc90410d4
hmhm903 <hmhm903m@gmail.com>
2017-11-07 19:22:41 +0900

    Add definition of Reversi class.

--- reversi/reversi.h
+++ reversi/reversi.h
@@ -1,5 +1,19 @@
 #pragma once
 
-class Reversi{
+#include <vector>
+using namespace std;
 
+class Board;
+class Player;
+
+class Reversi{
+private:
+	Board* board_;
+	vector<Player*> player_list_;
+
+public:
+	Reversi();
+	~Reversi();
+
+	void Initialize();
 };

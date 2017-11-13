5d00fb49916512c676b3cf79abbef4fb9435b500
hmhm903 <hmhm903m@gmail.com>
2017-11-13 18:20:21 +0900

    Set next player when initialize player_list.

--- reversi/reversi.cpp
+++ reversi/reversi.cpp
@@ -18,6 +18,11 @@ void Reversi::Initialize()
 	board_ = new Board();
 	player_list_.push_back(new Human(STONE_COLOR::BLACK));
 	player_list_.push_back(new Human(STONE_COLOR::WHITE));
+
+	// create player loop: black->white->black->white->...
+	for (vector<Player*>::iterator i = player_list_.begin(); i != player_list_.end() - 1; i++)
+		(*i)->SetNextPlayer(*(i + 1));
+	(*(player_list_.end() - 1))->SetNextPlayer(*player_list_.begin());
 }
 
 Board* Reversi::GetBoardPtr()

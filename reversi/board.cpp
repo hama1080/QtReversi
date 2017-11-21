9544efe261335004769fd5c7f17d5ab07dc98c51
hmhm903 <hmhm903m@gmail.com>
2017-11-21 21:02:03 +0900

    Create put_pos - reverse_pos map.

--- reversi/board.cpp
+++ reversi/board.cpp
@@ -1,6 +1,7 @@
 ﻿#include "board.h"
 #include "cell.h"
 #include <iostream>
+#include <iterator>
 using namespace std;
 
 map<Vec2d, vector<Vec2d>> Board::SearchPossiblePutPos(STONE_COLOR player_color)
@@ -15,10 +16,14 @@ map<Vec2d, vector<Vec2d>> Board::SearchPossiblePutPos(STONE_COLOR player_color)
 		}
 	}
 
+	map<Vec2d, vector<Vec2d>> put_reverse_map;
+
 	// check player_stone can put on empty cell
 	for (auto empty_cell : empty_cell_list)
 	{
 		//cout << "empty cell: " << empty_cell.first << ", " << empty_cell.second << endl;
+		vector<Vec2d> all_reverse_pos_list;
+
 		for (auto dir : kDirectionList)
 		{
 			Vec2d dir_pos(empty_cell.first + dir.first, empty_cell.second + dir.second);
@@ -29,6 +34,8 @@ map<Vec2d, vector<Vec2d>> Board::SearchPossiblePutPos(STONE_COLOR player_color)
 			//cout << "pos: " << pos.first << ", " << pos.second << endl;
 			if (board_[dir_pos].GetStoneColor() != player_color)
 			{
+				vector<Vec2d> reverse_pos_list;
+				reverse_pos_list.push_back(dir_pos);
 				for (unsigned int distance = 2; ; distance++)
 				{
 					Vec2d check_pos(empty_cell.first + dir.first * distance, empty_cell.second + dir.second * distance);
@@ -39,17 +46,25 @@ map<Vec2d, vector<Vec2d>> Board::SearchPossiblePutPos(STONE_COLOR player_color)
 
 					if (board_[check_pos].GetStoneColor() == player_color)
 					{
-						cout << "posiible_pos: " << empty_cell.first << ", " << empty_cell.second << endl;
+						//cout << "posiible_pos: " << empty_cell.first << ", " << empty_cell.second << endl;
+						std::copy(reverse_pos_list.begin(), reverse_pos_list.end(), std::back_inserter(all_reverse_pos_list));
 						break;
 					}
+					reverse_pos_list.push_back(check_pos);
 				}
 			}
-
 		}
 
-	}
+		if (all_reverse_pos_list.size() != 0) {
+			put_reverse_map[empty_cell] = all_reverse_pos_list;
 
-	return map<Vec2d, vector<Vec2d>>();
+			//test
+			cout << "posiible_pos: " << empty_cell.first << ", " << empty_cell.second << endl;
+			for (auto cell : all_reverse_pos_list)
+				cout << "reverse pos: " << cell.first << ", " << cell.second << endl;
+		}
+	}
+	return put_reverse_map;
 }
 
 Board::Board(unsigned int size_x, unsigned int size_y)

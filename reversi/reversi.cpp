8c186f20f0407350e012a308da574de28f1052e7
hmhm903 <hmhm903m@gmail.com>
2017-12-02 22:40:59 +0900

    Test computer vs computer.

--- reversi/reversi.cpp
+++ reversi/reversi.cpp
@@ -56,7 +56,7 @@ void Reversi::Initialize()
 {
 	board_ = new Board();
 	player_list_.push_back(new Computer(STONE_COLOR::BLACK));
-	player_list_.push_back(new Human(STONE_COLOR::WHITE));
+	player_list_.push_back(new Computer(STONE_COLOR::WHITE));
 
 	// create player loop: black->white->black->white->...
 	for (vector<Player*>::iterator i = player_list_.begin(); i != player_list_.end() - 1; i++)

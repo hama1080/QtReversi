a9911cf991cf968e7586717954db0ece660055ba
hmhm903 <hmhm903m@gmail.com>
2017-12-14 21:40:40 +0900

    Decide put position of computer randomly.

--- reversi/computer.cpp
+++ reversi/computer.cpp
@@ -1,5 +1,8 @@
 ﻿#include "computer.h"
 #include "board.h"
+#include <random>
+
+using namespace std;
 
 Computer::Computer(STONE_COLOR color)
 	:Player(color)
@@ -10,8 +13,14 @@ bool Computer::AskPutStonePosition(Vec2d& put_pos, Board* board)
 {
 	map<Vec2d, vector<Vec2d>> possible_pos_map = board->GetPossiblePutPos();
 
+	random_device rnd;
+	vector<Vec2d> possible_pos_vec;
+	size_t index = rnd() % possible_pos_map.size();
+	for (auto possible_pos : possible_pos_map)
+		possible_pos_vec.push_back(possible_pos.first);
+
 	// set put_pos
-	put_pos.first = possible_pos_map.begin()->first.first;		//tmp
-	put_pos.second = possible_pos_map.begin()->first.second;	//tmp
+	put_pos.first = (possible_pos_vec.begin() + index)->first;
+	put_pos.second = (possible_pos_vec.begin() + index)->second;
 	return false;
 }

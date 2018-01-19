a02a9c24ac7a0d1ce27a6d606b89d7af940f120b
hmhm903 <hmhm903m@gmail.com>
2018-01-19 22:52:20 +0900

    Add GetSelectedMode()

--- reversi_QtGUI/settingDialog.cpp
+++ reversi_QtGUI/settingDialog.cpp
@@ -5,3 +5,14 @@ SettingDialog::SettingDialog(QWidget* parent)
 {
 	setupUi(this);
 }
+
+Mode SettingDialog::GetSelectedMode()
+{
+	if (normal_radio->isChecked())
+		return Mode::Default;
+	else if (multi_radio->isChecked())
+		return Mode::Multi;
+	else if (largescale_radio->isChecked())
+		return Mode::LargeScale;
+	return Mode::Default;
+}
--- reversi_QtGUI/settingDialog.h
+++ reversi_QtGUI/settingDialog.h
@@ -1,10 +1,12 @@
 ﻿#pragma once
 #include <QDialog>
 #include "ui_setting.h"
+#include <common.h>
 
 class SettingDialog : public QDialog, public Ui::SettingDialog
 {
 Q_OBJECT
 public:
 	SettingDialog(QWidget* parent = 0);
+	Mode GetSelectedMode();
 };
